472e0e7fd4790bcf12366567e9956f64f04f8be9
Aravind <aravind@nittio.com>
2015-05-30 15:19:35 +0530

    Missed out in last

---
+++ www/_code/view_controllers/dashboard.js
@@ -0,0 +1,57 @@
+(function() {
+
+//-------------------------------------------------------------------------------------------------
+// dashboard.js:
+// home dashboard
+//-------------------------------------------------------------------------------------------------
+function module_init() {
+    angular.module('nl.dashboard', [])
+    .config(configFn)
+    .controller('nl.HomeCtrl', HomeCtrl);
+}
+
+//-------------------------------------------------------------------------------------------------
+var configFn = ['$stateProvider',
+function($stateProvider) {
+    $stateProvider.state('app.home', {
+        cache: true,
+        url : '/home',
+        views : {
+            'appContent' : {
+                templateUrl : 'lib_ui/cards/cardsview.html',
+                controller : 'nl.HomeCtrl'
+            }
+        }
+    });
+}];
+
+//-------------------------------------------------------------------------------------------------
+var HomeCtrl = ['nl', '$scope', '$stateParams', 'nlServerApi',
+function(nl, $scope, $stateParams, nlServerApi) {
+    $scope.title = 'Nittio Learn';
+    $scope.cards = nlServerApi.getDashboardCards();
+
+    $scope.onHold = function(event) {
+        console.log('onHold');
+        event.preventDefault();
+        return false;
+    };
+    $scope.onTap = function(event) {
+        console.log('onTap');
+        event.preventDefault();
+        return false;
+    };
+    $scope.onDoubleTap = function($event) {
+        console.log('onDoubleTap');
+        $event.preventDefault();
+        return false;
+    };
+    $scope.onTouch = function($event) {
+        console.log('onTouch');
+        $event.preventDefault();
+        return false;
+    };
+}];
+
+module_init();
+})();
