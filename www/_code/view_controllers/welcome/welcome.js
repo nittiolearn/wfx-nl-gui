8dbbd1ebb9d10f9b2a4c9072467754593aa37eec
Aravind <aravind@nittio.com>
2016-03-28 14:11:29 +0530

    team page + image optimizations

--- www/_code/view_controllers/welcome/welcome.js
+++ www/_code/view_controllers/welcome/welcome.js
@@ -150,7 +150,7 @@ function(nl, nlRouter, $scope, nlAnchorScroll) {
         title: nl.t(_commonMsg),
         desc: 'Online training software for businesses. Teaching Quality Management solutions for schools.',
         pageUrl: null,
-        bgImg: 'background.jpg',
+        bgImg: 'background2.jpg',
         menus: [],
         
         // Required in the specific view template
@@ -170,7 +170,7 @@ function(nl, nlRouter, $scope, nlAnchorScroll) {
         title: nl.t('Your teaching quality partner.'),
         desc: 'Looking to improve the teaching quality in your school? Use Nittio Learn for continuous teacher training, structured lesson planning, program of work tracking and classroom observations.',
         pageUrl: 'school',
-        bgImg: 'background.jpg',
+        bgImg: 'schoolbackground2.jpg',
         menus: [{name: 'Solutions', anchor: 'solutions'}, {name: 'Contact us', anchor: 'contact_us'}],
         
         // Required in the specific view template
@@ -190,7 +190,7 @@ function(nl, nlRouter, $scope, nlAnchorScroll) {
         title: nl.t('Online Training Management Software.'),
         desc: 'Online software for training everyone in your company. Reduce your training costs, measure results and stay on top of learning needs of your organization.',
         pageUrl: 'business',
-        bgImg: 'businessbackground.png',
+        bgImg: 'businessbackground2.jpg',
         menus: [{name: 'Features', anchor: 'features'}, {name: 'Pricing', anchor: 'pricing'}, {name: 'Request a demo', anchor: 'contact_us'}],
         
         // Required in the specific view template
@@ -201,11 +201,11 @@ function(nl, nlRouter, $scope, nlAnchorScroll) {
                 'Customized dashboards', 'Custom landing page', 'Sell courses', 
                 'Reporting', 'Support', 'Custom contracts', 'Migration services'],
             pricing_slabs: [
-            {name: 'Basic', price: 'USD 124 per month', billing: '(Billed annually)', 
+            {name: 'Basic', price: 'USD 125 per month', billing: '(Billed annually)', 
                 users: '50', attrs: ['tick', 'tick', 'tick', 'tick', '', '', '', '', 'tick', "Email", '', '']},
-            {name: 'Advanced', price: 'USD 211 per month', billing: '(Billed annually)',
+            {name: 'Advanced', price: 'USD 225 per month', billing: '(Billed annually)',
                 users: '100', attrs: ['tick', 'tick', 'tick', 'tick', 'tick', 'tick', '', '', 'tick', "Email, Phone", '', '']},
-            {name: 'Professional', price: 'USD 622 per month', billing: '(Billed annually)', 
+            {name: 'Professional', price: 'USD 625 per month', billing: '(Billed annually)', 
                 users: '500', attrs: ['tick', 'tick', 'tick', 'tick', 'tick', 'tick', 'tick', 'tick', "Custom reports", "Email, Phone", '', '']},
             {name: 'Enterprise', price: 'Contact us for pricing', billing: ' ',
                 users: '500+', attrs: ['tick', 'tick', 'tick', 'tick', 'tick', 'tick', 'tick', 'tick', "Custom reports", "Named account manager", 'tick', 'tick']}
@@ -238,38 +238,37 @@ function(nl, nlRouter, $scope, nlAnchorScroll) {
             msg: '',
             firstPageClass: 'bgdark',
             profiles: [
-                { id: 'gagan', name: 'Gagandeep Josan', role: 'Co-founder & CEO', desc: [
-                    "Jack of all trades and master of a few of them. When not thinking about the company strategy and growth, Gagan spends his time acquiring new customers and making contributions to the technology team.",
-                    "With more than 17 years of experience, a bulk of it managing products for  companies like Nokia and Amazon, Gagan brings in a mix of business and technology expertise. In his free time and while driving, Gagan likes listening to a variety of podcasts and catching up on popular American crime drama TV series.",
+                { id: 'gagan2', name: 'Gagandeep Josan', role: 'Co-founder & CEO', desc: [
+                    "Gagan leads the work on company growth strategy. A polymath, he contributes to all aspects of the company - from acquiring new customers to developing great new features in the product.",
+                    "With more than 17 years of experience, a bulk of it managing products for  companies like Nokia and Amazon, Gagan brings in a mix of business and technology expertise. In his free time, Gagan likes listening to a variety of podcasts and catching up on popular American crime drama TV series.",
                     "Gagan has an engineering degree from Thapar Institute of Engineering and Technology."
                 ]},
-                { id: 'ritu', name: 'Ritu Josan', role: 'Co-founder', desc: [
-                    "A great product still needs a messenger who can connect it to its potential users through simple messaging. For Nittio Learn, that messenger is Ritu. Ritu leads Nittio's customer acquisition and growth. With the uncommon ability of listening to customer, she ensures that they derive the maximum value from using Nittio Learn.",
-                    "Ritu got her engineering degree from NIT Jamshedpur but found her calling in marketing strategy. Ritu has over fourteen years of sales and marketing experience across industries and geographies. As a marketing  consultant, she provided digital marketing strategy to brands like Citibank,  Wipro.",
+                { id: 'ritu2', name: 'Ritu Josan', role: 'Co-founder and Chief Evangelizer', desc: [
+                    "A great product still needs a messenger who can connect it to its potential users through simple messaging. For Nittio Learn, that messenger is Ritu. Ritu leads Nittio's customer acquisition and growth. Ritu wears the customer hat in all internal discussions to ensure that our customers derive the maximum value from using Nittio Learn.",
+                    "Ritu got her engineering degree from NIT Jamshedpur but found her calling in marketing strategy. Ritu has over fourteen years of sales and marketing experience across industries and geographies. As a marketing  consultant, she provided digital marketing strategy to brands like Citibank, Wipro.",
                     "Ritu unwinds by spending her spare time with her children, Aarav and Anika and by watching English comedies."
                 ]},
-                { id: 'aravind', name: 'Aravindan RS', role: 'Co-founder & CTO', desc: [
+                { id: 'aravind2', name: 'Aravindan RS', role: 'Co-founder & CTO', desc: [
                     "Aravind breaths technology and lives processes. Throw an ill-defined, complex problem, technical or non-technical at him and he hands you back clarity in problem definition along with well structured multiple solutions. Aravind strives for functional simplicity in everything that he does. No wonder that the Nittio Learn content creation and learning management capabilities often get complimented as being the simplest ones around.",
                     "An alumni of IIT Kharagpur with 20 years of large scale software architecture experience at Nokia Networks under his belt, Aravind loves spending his free time playing football with his two children, Shishir and Nila."
                 ]},
-                { id: 'ruchi', name: 'Ruchita Gupta', role: 'Customer Success Manager', desc: [
+                { id: 'ruchi2', name: 'Ruchita Gupta', role: 'Customer Success Lead', desc: [
                     "Ruchi's speed at work would put flash to shame. Whether it is rolling out a new project or handling a customer request, Ruchi\'s responsiveness combined with her meticulousness has delighted Nittio Learn customers time and again.",
                     "Ruchi has the distinciton of launching India's first  podcast, www.podmasti.com in 2005. Ruchi loves tutoring her son Tanish and takes out time everyday for it."
                 ]},
-                { id: 'puneet', name: 'Puneet Kamboj', role: 'Content Manager', desc: [
-                    "The content expert in the team who takes time out to find ways to needle the development team by finding bugs in the new software releases. Throw any challenge at her and she comes out trumps. Judicious in her use of words, Puneet lets her work do the talking instead.",
-                    "After completing her Masters in Chemistry, Puneet taught at schools before joining Nittio. Her education experience combined with her knack for technology is helping Nittio provide differentiated and innovative content."
+                { id: 'puneet2', name: 'Puneet Kamboj', role: 'Product Verification Lead', desc: [
+                    "The software verification expert intent on needling the development team by finding bugs, Puneet also loves creating beautiful content. Judicious in her use of words, Puneet lets her work do the talking instead. Throw any challenge at her and she comes out trumps.",
+                    "After completing her Masters in Chemistry, Puneet taught at schools before joining Nittio. Her education experience combined with her knack for technology has helped build Nittio Learn into a highly effective learning environment."
                 ]},
-                { id: 'sarada', name: 'Sarada Veerabhatla', role: 'Content Manager', desc: [
-                    "With Masters degree from IIT Delhi under her belt, Sarada has all it takes to ensure that the quality of software and content produced in Nittio is always top notch. Sarada's ability to break down any problem given to her and come up with an execution plan has helped Nittio continuously better both technology and content.",
-                    "Mom of two, Sarada loves spending time with her kids."
+                { id: 'sarada2', name: 'Sarada Veerabhatla', role: 'Content Lead', desc: [
+                    "The simplicity of Nittio Learn self-learning content can easily deceive a person about the complexity of the content creation work. With Masters degree from IIT Delhi under her belt, Sarada leads the content development charge at Nittio. Sarada's ability to break down any problem given to her and come up with an execution plan has helped Nittio continuously better both technology and content."
                 ]},
-                { id: 'varsha', name: 'Varsha Shrote', role: 'Customer Success Manager', desc: [
+                { id: 'varsha2', name: 'Varsha Shrote', role: 'Customer Success Lead', desc: [
                     "Varsha works as part of the Customer Success team ensuring that there is no hitch in their experience while using Nittio Learn.",
                     "Varsha has an Electrical Engineering degree from Nagpur University and a Post Graduate Diploma in Advanced Computing from Centre for Development of Advanced Computing (C-DAC), Bangalore.",
                     "She is passionate about travelling and reading.  In her free time she likes to experiment with new recipes from different regions."
                 ]},
-                { id: 'naveen', name: 'Naveen Kumar', role: 'Software Developer', desc: [
+                { id: 'naveen2', name: 'Naveen Kumar', role: 'Software Developer', desc: [
                     "Naveen is part of our technology team. He works on the latest product features.",
                     "Naveen has an engineering degree in electronics and communication. In his free time Naveen loves to work at his silk worm farm."
                 ]}
