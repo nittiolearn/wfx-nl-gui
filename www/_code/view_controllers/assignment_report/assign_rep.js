e64fe8e1199957688c62272566c8ee7d1f91c7b1
naveen@nittio.com <naveen@nittio.com>
2017-05-26 18:24:18 +0530

    #694: Remove all usages of ivh-tree view library

--- www/_code/view_controllers/assignment_report/assign_rep.js
+++ www/_code/view_controllers/assignment_report/assign_rep.js
@@ -102,17 +102,17 @@ function TypeHandler(reptype, nl, nlServerApi, nlDlg) {
 function getController(ctrlType) {
 	return [
 		'nl', 'nlRouter', '$scope', 'nlDlg', 'nlCardsSrv', 'nlServerApi', 'NlAssignReportStats',
-		'nlGroupInfo',
+		'nlGroupInfo', 'nlOuUserSelect',
 		function(nl, nlRouter, $scope, nlDlg, nlCardsSrv, nlServerApi, NlAssignReportStats,
-			nlGroupInfo) {
+			nlGroupInfo, nlOuUserSelect) {
 	    _assignRepImpl(ctrlType, nl, nlRouter, $scope, nlDlg, nlCardsSrv, nlServerApi, 
-	    	NlAssignReportStats, nlGroupInfo);
+	    	NlAssignReportStats, nlGroupInfo, nlOuUserSelect);
 	}];
 }
 
 //-------------------------------------------------------------------------------------------------
 function _assignRepImpl(reptype, nl, nlRouter, $scope, nlDlg, nlCardsSrv, nlServerApi, 
-	NlAssignReportStats, nlGroupInfo) {
+	NlAssignReportStats, nlGroupInfo, nlOuUserSelect) {
 	var _userInfo = null;
 	var my = 0;
 	var search = null;
@@ -355,8 +355,8 @@ function _assignRepImpl(reptype, nl, nlRouter, $scope, nlDlg, nlCardsSrv, nlServ
         _populateLinks(linkAvp);
         if (reptype == 'assignment') nl.fmt.addAvp(avps, 'Name', report.name);
         nl.fmt.addAvp(avps, 'Number of reports', lst.length);
-        nl.fmt.addAvp(avps, 'Most recent update', nl.fmt.fmtDateDelta(lst[0].updated, now));
-        nl.fmt.addAvp(avps, 'Earliest update', nl.fmt.fmtDateDelta(lst[lst.length-1].updated, now));
+        nl.fmt.addAvp(avps, 'Most recent update', lst[0].updated, 'datedelta');
+        nl.fmt.addAvp(avps, 'Earliest update', lst[lst.length-1].updated, 'datedelta');
         if (reptype != 'assignment') return avps;
         reportStats.populateCommonAvps(report, avps);
 		return avps;
@@ -442,75 +442,104 @@ function _assignRepImpl(reptype, nl, nlRouter, $scope, nlDlg, nlCardsSrv, nlServ
         }
         NlAssignReportStats.export($scope, reportStats.getRecords(), _userInfo);
     }
-    
+
+    var _ouUserSelector = null;
+    var _selectedUsers = {};
+    var _selectedUserIdsObj = {};
+     
 	function _assignmentShare($scope, card){
 		var data = {repid: card.id};
 		var sharedUsersList = [];
-		nlServerApi.assignmentSharedUsers(data).then(function(status){
-			sharedUsersList = status;	
-			var assignmentShareDlg = nlDlg.create($scope);
-			_initAssignmentShareDlg(assignmentShareDlg, sharedUsersList);
-			assignmentShareDlg.scope.removeSharedUser = function(id){
-				var alreadySharedUsers = assignmentShareDlg.scope.data.sharedUsers;
-				for(var i in alreadySharedUsers){
-					var card = alreadySharedUsers[i];
-					if(card.id == id) {
-						assignmentShareDlg.scope.data.sharedUsers.splice(i, 1);
-						var index = assignmentShareDlg.scope.data.selectedSharedUserListIds.indexOf(id);
-						assignmentShareDlg.scope.data.selectedSharedUserListIds.splice(index, 1);
-					}
-				}
-				_updateVisibalityToUsers(alreadySharedUsers, assignmentShareDlg);
-			};
-				
-			var updateButton =  {text : nl.t('Update'), onTap: function(e){
-				if(e) e.preventDefault();
-				var data = {repid: card.id,
-							sharedUsers: assignmentShareDlg.scope.data.selectedSharedUserListIds};
-				nlServerApi.assignmentUpdateSharedUsers(data).then(function(status){
-					assignmentShareDlg.close(false);
-				});
-			}};				
-			var cancelButton = {
-				text : nl.t('Cancel')
-			};
-				
-			assignmentShareDlg.scope.onSharedUserSelect = function(){
-				_showOuListDlg($scope, assignmentShareDlg, sharedUsersList);
-			};	
-			assignmentShareDlg.show('view_controllers/assignment_report/assignment_share_dlg.html', [updateButton], cancelButton, false);
-		});
+		var assignmentShareDlg = nlDlg.create($scope);
+        var alreadySharedUsers = [];
+			assignmentShareDlg.scope.data = {};
+			assignmentShareDlg.scope.data.card = card;
+        nlGroupInfo.init().then(function() {
+            nlGroupInfo.update();
+            _ouUserSelector = nlOuUserSelect.getOuUserSelector($scope, 
+                nlGroupInfo.get(), {}, {});
+			nlServerApi.assignmentSharedUsers(data).then(function(status){
+				sharedUsersList = status;	
+				_initAssignmentShareDlg(assignmentShareDlg, sharedUsersList);
+	        	_showDlg(assignmentShareDlg, sharedUsersList, alreadySharedUsers);			
+			});
+        });
 	};
+
+	function _showDlg(assignmentShareDlg, sharedUserList, alreadySharedUsers){
+
+		assignmentShareDlg.scope.removeSharedUser = function(id){
+			_removeSelectedUser(assignmentShareDlg, id);
+		};
+				
+		var updateButton =  {text : nl.t('Update'), onTap: function(e){
+			if(e) e.preventDefault();
+			var data = {repid: assignmentShareDlg.scope.data.card.id,
+						sharedUsers: assignmentShareDlg.scope.data.selectedSharedUserListIds};
+			nlServerApi.assignmentUpdateSharedUsers(data).then(function(status){
+				assignmentShareDlg.close(false);
+			});
+		}};
+		
+		var cancelButton = {
+			text : nl.t('Cancel')
+		};
+			
+		assignmentShareDlg.scope.onSharedUserSelect = function(){
+			_showOuListDlg($scope, assignmentShareDlg, sharedUserList);
+		};	
+		assignmentShareDlg.show('view_controllers/assignment_report/assignment_share_dlg.html', [updateButton], cancelButton, false);
+	}
+
+	function _removeSelectedUser(assignmentShareDlg, id){
+		var alreadySharedUsers = assignmentShareDlg.scope.data.sharedUsers;
+		for(var i in alreadySharedUsers){
+			var card = alreadySharedUsers[i];
+			if(card.id == id) {
+				assignmentShareDlg.scope.data.sharedUsers.splice(i, 1);
+				var index = assignmentShareDlg.scope.data.selectedSharedUserListIds.indexOf(id);
+				assignmentShareDlg.scope.data.selectedSharedUserListIds.splice(index, 1);
+			}
+		}
+		_updateVisibalityToUsers(alreadySharedUsers, assignmentShareDlg);
+	}
 
 	function _initAssignmentShareDlg(assignmentShareDlg, sharedUsersList){
-			assignmentShareDlg.setCssClass('nl-height-max nl-width-max');
-			assignmentShareDlg.scope.data = {};
-			assignmentShareDlg.scope.data.users = sharedUsersList;
-			assignmentShareDlg.scope.data.removeicon = nl.url.resUrl('quick-links/close.png');
-			assignmentShareDlg.scope.data.sharedWith = nl.t('Not shared with any user:');
-			assignmentShareDlg.scope.data.sharedUsersIds = _updateOuTree(sharedUsersList, assignmentShareDlg);
-			assignmentShareDlg.scope.data.sharedUsers = assignmentShareDlg.scope.data.sharedUsersIds[0];
-			assignmentShareDlg.scope.data.selectedSharedUserListIds = assignmentShareDlg.scope.data.sharedUsersIds[1];				
-			assignmentShareDlg.scope.treeOptions = {
-				defaultSelectedState : false,
-				twistieExpandedTpl : nl.fmt2('<img src="{}" class="nl-16">', nl.url.resUrl('folder.png')),
-				twistieCollapsedTpl : nl.fmt2('<img src="{}" class="nl-16">', nl.url.resUrl('folder_closed.png')),
-				twistieLeafTpl : nl.fmt2('<img src="{}" class="nl-16">', nl.url.resUrl('file.png')),
-				labelAttribute : 'text'
-			};
+		assignmentShareDlg.setCssClass('nl-height-max nl-width-max');
+		assignmentShareDlg.scope.data.users = sharedUsersList;
+		assignmentShareDlg.scope.data.removeicon = nl.url.resUrl('quick-links/close.png');
+		assignmentShareDlg.scope.data.sharedWith = nl.t('Not shared with any user:');
+		assignmentShareDlg.scope.data.sharedUsersIds = _updateOuTree(sharedUsersList, assignmentShareDlg);
+		assignmentShareDlg.scope.data.sharedUsers = assignmentShareDlg.scope.data.sharedUsersIds[0];
+		assignmentShareDlg.scope.data.selectedSharedUserListIds = assignmentShareDlg.scope.data.sharedUsersIds[1];				
+		assignmentShareDlg.scope.data.ouUserTree =_ouUserSelector.getTreeSelect();
+		_selectedUsers = _getSelectedUsers(assignmentShareDlg.scope.data.ouUserTree);
+        _ouUserSelector.updateSelectedIds(_selectedUsers);
 	};
 
+	function _getSelectedUsers(userTree){
+		var selectedUsers = {};
+		for(var i=0; i< userTree.data.length; i++){
+			var user = userTree.data[i];
+			if(!user.userObj) continue;
+			if(user.userObj.id in _selectedUserIdsObj) selectedUsers[user.id] = user;
+		};
+		return selectedUsers;
+	};
+	
 	function _updateOuTree(userData, assignmentShareDlg) {
 		var alreadySharedUsers = [];
 		var alreadySharedUsersIds = [];
 		var sharedUsersAndIds = [];
+			_selectedUserIdsObj = {};
 		for (var i in userData) {
 			var node = userData[i];
 			for(var j in node.children){
 				var subnode = node.children[j];
 				if(subnode.shared == true){
 					alreadySharedUsers.push(subnode);
-					alreadySharedUsersIds.push(subnode.id);		
+					alreadySharedUsersIds.push(subnode.id);
+					_selectedUserIdsObj[subnode.id] = subnode;		
 				}
 			}
 		}
@@ -539,45 +568,44 @@ function _assignRepImpl(reptype, nl, nlRouter, $scope, nlDlg, nlCardsSrv, nlServ
 	
 	
 	function _showOuListDlg(parentScope, assignmentShareDlg, ouList){
-				var ouSelectionDlg = nlDlg.create(parentScope);
-			_initouSelectionDlg(ouSelectionDlg, ouList);
+		var ouSelectionDlg = nlDlg.create(parentScope);
+		ouSelectionDlg.scope.data = {};
+        _ouUserSelector.updateSelectedIds(_selectedUsers);
+		_initouSelectionDlg(ouSelectionDlg, assignmentShareDlg);
 
-		function _initouSelectionDlg(ouSelectionDlg, ouList) {
-			ouSelectionDlg.setCssClass('nl-height-max nl-width-max');	
-			ouSelectionDlg.scope.treeData = ouList;
-			ouSelectionDlg.scope.treeOptions = {
-			defaultSelectedState: false,
-			twistieExpandedTpl: nl.fmt2('<img src="{}" class="nl-16">', nl.url.resUrl('folder.png')),
-			twistieCollapsedTpl: nl.fmt2('<img src="{}" class="nl-16">', nl.url.resUrl('folder_closed.png')),
-			twistieLeafTpl: nl.fmt2('<img src="{}" class="nl-16">', nl.url.resUrl('file.png')),
-		    labelAttribute: 'text'
-			};
-		}
-
-		ouSelectionDlg.scope.onNodeClick = function(node, isSelected, tree) {
-			assignmentShareDlg.scope.data.sharedUsers = _updateOuTree(ouSelectionDlg.scope.treeData);
-		};
-
-		function _updateOuTree(userData) {
+		var addButton = {text : nl.t('Add'), onTap: function(){
+			if(Object.keys(ouSelectionDlg.scope.data.org_unit.selectedIds).length == 0) return;			
+			assignmentShareDlg.scope.data.sharedUsersIds = _updateSelectedUsersAndIds(ouSelectionDlg.scope.data.org_unit.selectedIds);
+			assignmentShareDlg.scope.data.sharedUsers = assignmentShareDlg.scope.data.sharedUsersIds[0];
+			assignmentShareDlg.scope.data.selectedSharedUserListIds = assignmentShareDlg.scope.data.sharedUsersIds[1];
+			_selectedUsers = ouSelectionDlg.scope.data.org_unit.selectedIds;
+		}};
+		var cancelButton = {text : nl.t('Cancel')};
+		
+		function _updateSelectedUsersAndIds(selectedIds){
 			var alreadySharedUsers = [];
-			assignmentShareDlg.scope.data.selectedSharedUserListIds = [];
-			for (var i in userData) {
-				var node = userData[i];
-				for(var j in node.children){
-					var subnode = node.children[j];
-					if(subnode.shared == true || subnode.selected == true){
-						alreadySharedUsers.push(subnode);
-						assignmentShareDlg.scope.data.selectedSharedUserListIds.push(subnode.id);					
-					}
-				}
+			var alreadySharedUsersIds = [];
+			var sharedUsersAndIds = [];
+			for (var i in selectedIds) {
+				var node = selectedIds[i];
+					var userObject = {container:node.userObj.org_unit, id: node.userObj.id, text: node.name};
+					alreadySharedUsers.push(userObject);
+					alreadySharedUsersIds.push(node.userObj.id);		
 			}
 			_updateVisibalityToUsers(alreadySharedUsers, assignmentShareDlg);
-			return alreadySharedUsers;
+			sharedUsersAndIds.push(alreadySharedUsers, alreadySharedUsersIds);
+			return sharedUsersAndIds;
 		}
-		var cancelButton = {text : nl.t('Add')};
+
 		ouSelectionDlg.show('view_controllers/assignment_report/share_report_dlg.html',
-			[], cancelButton, false);
+			[addButton], cancelButton, false);
 	}
+	
+	function _initouSelectionDlg(ouSelectionDlg, assignmentShareDlg) {
+		ouSelectionDlg.setCssClass('nl-height-max nl-width-max');
+        ouSelectionDlg.scope.data.org_unit = assignmentShareDlg.scope.data.ouUserTree;
+	}
+
 }
 
 //-------------------------------------------------------------------------------------------------
