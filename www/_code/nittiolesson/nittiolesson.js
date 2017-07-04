96303769fb8925f83cc50420434dbc3c519343ac
naveen@nittio.com <naveen@nittio.com>
2017-07-04 17:32:25 +0530

    third commit for module props to angular

--- www/_code/nittiolesson/nittiolesson.js
+++ www/_code/nittiolesson/nittiolesson.js
@@ -10,66 +10,113 @@ function module_init() {
 }
 
 //-------------------------------------------------------------------------------------------------
-var NittioLessonSrv = ['nl', 'nlDlg', 'nlServerApi',
-function(nl, nlDlg, nlServerApi) {
+var NittioLessonSrv = ['nl', 'nlDlg',
+function(nl, nlDlg) {
 	var _oLesson = null;
 	var _dropdownOptions = null;
-	
+	var _isPdf = false;
 	this.init = function(oLesson, dropdownOptions) {
-		_oLesson = _oLesson;
+		_oLesson = oLesson;
 		_dropdownOptions = dropdownOptions;
 	};
-		 
-	this.showModulePropertiesDlg = function(isCreate) {
-		var doneButtonText = isCreate ? 'Create' : 'Done';
-		return nl.q(function(resolve, reject) {
-			nlDlg.popupStatus('TODO');
-			resolve(false);
-		});
-	};
 
-	var moduleProps = [{id: 'name', name: nl.t('Name'), type: 'string'},
+	var moduleProps = [{id: 'name', name: nl.t('Name'), type: 'string', condition: 'isModule'},
 					   {id: 'grade', name: nl.t('Grade'), type: 'select'},
 					   {id: 'subject', name: nl.t('Subject'), type: 'select'},
 					   {id: 'image', name: nl.t('Image'), type: 'select'},
+					   {id: 'pdfUrl', name: nl.t('Pdf url'), type: 'string', condition: 'isPdf'},
+					   {id: 'singlePage', name: nl.t('Pdf url'), type: 'check', condition: 'isPdf'},
 					   {id: 'description', name: nl.t('Description'), type: 'textarea'},
 					   {id: 'keywords', name: nl.t('Keywords'), type: 'string'},
 					   {id: 'esttime', name: nl.t('Estimated time'), type: 'number'},
-					   {id: 'custtype', name: nl.t('Content type'), type: 'select'},
+					   {id: 'content_type', name: nl.t('Module type'), type: 'select'},
+					   {id: 'passScore', name: nl.t('Pass score %'), type: 'number'},
+					   {id: 'allowed_max_score', name: nl.t('Score limit'), type: 'number', condition: 'isQuestionBank'},
 					   {id: 'forumTopic', name: nl.t('Discussion topic'), type: 'string'},
-					   {id: 'additional_attr', name: nl.t('Template attributes:'), type:'additional'},
-					   {id: 'templateStylesCss', name: nl.t('Styles'), type: 'textarea'},
-					   {id: 'templateBgimgs', name: nl.t('Background images'), type: 'textarea'},
-					   {id: 'templatePageTypes', name: nl.t('Page types'), type: 'textarea'},
-					   {id: 'templateAnimations', name: nl.t('Animation schemes'), type: 'textarea'}];
+					   {id: 'additional_attr', name: nl.t('Template attributes:'), type: 'group'},
+					   {id: 'templateStylesCss', name: nl.t('Styles'), type: 'textarea', group:'additional_attr'},
+					   {id: 'templateBgimgs', name: nl.t('Background images'), type: 'textarea', group:'additional_attr'},
+					   {id: 'templatePageTypes', name: nl.t('Page types'), type: 'textarea', group:'additional_attr'},
+					   {id: 'templateAnimations', name: nl.t('Animation schemes'), type: 'textarea', group:'additional_attr'},
+					   {id: 'lessonState', name: nl.t('Status'), type: 'status', group:'additional_attr'}];
 	
 	var learningMode = [{id:'assesment', name:nl.t('Assesment module')},
 						{id:'self', name: nl.t('Self learning module')}];
-	this.show = function(parentScope, lesson) {
-		return nl.q(function(resolve, reject){
-			console.log(lesson);
+
+	var lessonStates = [{title: 'Private', icon : nl.url.resUrl('general/ball-grey.png'), cls : 'grey'},
+                     	{title: 'Under review', icon : nl.url.resUrl('general/ball-yellow.png'), cls : 'yellow'},
+                     	{title: 'Under revision', icon : nl.url.resUrl('general/ball-maroon.png'), cls : 'maroon'},
+                     	{title: 'Approved', icon : nl.url.resUrl('general/ball-green.png'), cls : 'green'},
+                     	{title: 'Approved, next update under revision', icon : nl.url.resUrl('general/ball-green.png'), cls : 'green'},
+                     	{title: 'Approved, next update under review', icon : nl.url.resUrl('general/ball-green.png'), cls : 'green'}];
+
+	var lessonStatusDesc = nl.t('<div><span><b>State of the module can be one of following:</b></span></div>' +
+	 							'<div><ul><li><img src="{}"/> Private: Module created and not yet shared for review.</li>' + 
+								'<li><img src="{}"/> Under review: Author has shared the module for review.</li>' +
+								'<li><img src="{}"/> Under revision: Author is reworking on the module after review.</li>' + 
+								'<li><img src="{}"/> Approved: Module is approved.</li>' + 
+								'<li><img src="{}"/> Approved, next update under revision: Module is approved and the author is working on next update.</li>' +
+								'<li><img src="{}"/> Approved, next update under review: Module is approved and the next update is shared for review.</li></ul></div>', 
+								lessonStates[0].icon, lessonStates[1].icon, lessonStates[2].icon, lessonStates[3].icon, lessonStates[4].icon, lessonStates[5].icon);
+	var moduleHelp = {
+					  name: {desc: nl.t('<span>Mandatory - enter a name for your module.</span>')},
+					  grade: {desc: nl.t('<span>Choose the grade for which this module is created.</span>')},
+					  subject: {desc: nl.t('<span>Choose the subject for which this module is created.</span>')},
+					  image: {desc: nl.t('<span><ul><li>Choose the icon that will be displayed when this module is searched. Try <b>Custom</b> when you are not satisfied with the provided options.</li>' + 
+					  					 '<li>If you choose <b>"Custom"</b> in image field, you can enter URL (link) of the custom image. The URL could be an image uploaded within the system or a link to external source in internet</li></ul></span>')},
+					  pdfUrl: {desc: nl.t('<span><b>Mandatory:</b> Enter the url uploaded pdf.')},
+					  description: {desc: nl.t('<span>Write a description which will help others in the group to understand the purpose of this module.</span>')},
+					  keywords: {desc: nl.t('<span>Some keywords to help in search. You could use this categorize your content.</span>')},
+					  esttime: {desc: nl.t('<span>Estimated time in minutes (between 1 and 600) to complete this learning module. When this learning module is sent as an assignment, the assignment duration is per-filled with the estimated time if available.</span>')},
+					  content_type: {desc: nl.t('<span>Selecting the module type to self learining module or assesment module.</span>')},
+					  forumTopic: {desc: nl.t('<span>provide name of the discussion topic which should be displayed when the learner clicks on discussion forum icon from this lesson. This could be further change at page level. This field is the default at lesson level.</span>')},
+					  passScore: {desc: nl.t('<span> Minimum pass score in percentage (between 0 and 100 - both inclusive) for passing this module. Set this to zero to consider any score as passed. Clean the value to go back to the default value.</span>')},
+					  templateStylesCss: {desc: nl.t('<span>define custom CSS classes to be used in styling the module content.</span>')},
+					  templateBgimgs: {desc: nl.t('<span>Provide a list of background images (to be displayed in change look dialog) as a JSON array of strings.</span>')},
+					  templatePageTypes: {desc: nl.t('<span>Provide page types and layout as a JSON object.</span>')},
+					  templateAnimations: {desc: nl.t('<span>Provide animation schemes as a JSON object.</span>')},
+					  lessonState: {desc: lessonStatusDesc},
+					  allowed_max_score: {desc: nl.t('This parameter is specific to question bank. When the question bank is distributed, a random subset of questions will be chosen from the bank. The total score of the chosen questions will be kept equal to the score limit. If the maximum scores are different for different pages, it is possible that the maximum score of chosen pages is slightly less than the score limit.')}
+					 };	 
+
+	this.showModulePropertiesDlg = function(isCreate, bFromPdf) {
+		_isPdf = bFromPdf;
+		var doneButtonText = isCreate ? nl.t('Create') : nl.t('Done');
+		var parentScope = nl.rootScope;
+		return nl.q(function(resolve, reject) {
 			var editDlg = nlDlg.create(parentScope);
-			editDlg.setCssClass('nl-height-max nl-width-max');
-			_initEditDlg(editDlg, lesson);			
-			editDlg.scope.dlgTitle = nl.t('Module properties');
-			var okButton = {text: nl.t('Done'), onTap: function(e, lesson) {
-				// if(!_validateInputs(editDlg.scope)) {
-					// return;
-				// }
-				editDlg.scope.data.lessonData.grade = editDlg.scope.data.grade.id;
-				editDlg.scope.data.lessonData.subject = editDlg.scope.data.subject.id;
-				if(editDlg.scope.data.learningType.id == 'self') {
-					editDlg.scope.data.lessonData.selfLearningMode = true;
-				} else {
-					editDlg.scope.data.lessonData.selfLearningMode = false;
+				editDlg.setCssClass('nl-height-max nl-width-max');
+				_initEditDlg(editDlg);			
+				editDlg.scope.dlgTitle = nl.t('Module properties');
+				
+				editDlg.scope.data.imageChange = function() {
+					editDlg.scope.data.imageConvert = nl.url.lessonIconUrl(editDlg.scope.data.image.id);		
+				};
+			var okButton = {text: doneButtonText, onTap: function(e) {
+				if(!_validateInputs(editDlg.scope)) {
+					if (e) e.preventDefault();
+					return;
 				}
-				console.log(editDlg.scope.data.lessonData);
-				resolve(editDlg.scope.data.lessonData);
+				_oLesson.grade = editDlg.scope.data.grade.id;
+				_oLesson.subject = editDlg.scope.data.subject.id;
+				_oLesson.pdfSinglePage = editDlg.scope.data.pdfSinglePage;
+				if (editDlg.scope.data.image.id == "Custom") {
+					_oLesson.image = "img:" + editDlg.scope.data.imageUrl;
+				} else {
+					_oLesson.image = editDlg.scope.data.image.id;
+				};
+				
+				if(editDlg.scope.data.learningType.id == 'self') {
+					_oLesson.selfLearningMode = true;
+				} else {
+					delete _oLesson.selfLearningMode;
+				}
+				resolve(true);
 			}};
 			var cancelButton = {text: nl.t('Close'), onTap: function() {
 				resolve(false);
 			}};
-			editDlg.show('view_controllers/old_code_transform/edit_module_properties/edit_module_dlg.html', [okButton], cancelButton);
+			editDlg.show('nittiolesson/module_props_dlg.html', [okButton], cancelButton);
 		});
 	};
 
@@ -77,6 +124,21 @@ function(nl, nlDlg, nlServerApi) {
     	scope.error = {};
     	var data = scope.data.lessonData;
         if(!data.name) return _validateFail(scope, 'name', 'Module name is mandatory');
+        if(_isPdf && !data.pdfUrl) return _validateFail(scope, 'pdfUrl', 'Pdf url is mandatory');
+        if(!_validateJsonField(data.templateBgimgs)) return _validateFail(scope, 'templateBgimgs', 'Error while parsing json');
+        if(!_validateJsonField(data.templatePageTypes)) return _validateFail(scope, 'templatePageTypes', 'Error while parsing json');
+        if(!_validateJsonField(data.templateAnimations)) return _validateFail(scope, 'templateAnimations', 'Error while parsing json');        
+        
+        return true;
+    }
+
+    function _validateJsonField(jsonData) {
+    	if (!jsonData) return true;
+        try {
+            JSON.parse(jsonData);
+        } catch (e) {
+            return false;
+        }
         return true;
     }
 
@@ -85,30 +147,42 @@ function(nl, nlDlg, nlServerApi) {
         	nl.t(errMsg));
     }
 
-	function _initEditDlg(editDlg, lesson) {
-		nlServerApi.getUserInfoFromCacheOrServer().then(function(userInfo){
-			_userInfo = userInfo;
-			console.log(userInfo);
-			editDlg.scope.data = {};
-			editDlg.scope.data.moduleProps = moduleProps;
-			lesson.esttime = parseInt(lesson.esttime);
-			editDlg.scope.data.lessonData = lesson;
-			editDlg.scope.options = {grade: _getDropDown(_userInfo.groupinfo.grades), 
-									 subject: _getDropDown(_userInfo.groupinfo.subjects),
-									 learningMode: learningMode};
-			editDlg.scope.data.grade = {id: lesson.grade, name: lesson.grade};
-			editDlg.scope.data.subject = {id: lesson.subject, name: lesson.subject};						 
-			editDlg.scope.data.learningType = lesson.selfLearningMode || lesson.selfLearningMode == true ? learningMode[1] : learningMode[0]; 
-		});
+	function _initEditDlg(editDlg) {
+		editDlg.scope.data = {};
+		editDlg.scope.data.moduleProps = moduleProps;
+		editDlg.scope.data.isModule = true;
+		editDlg.scope.data.isPdf = _isPdf;
+		editDlg.scope.data.isQuestionBank = false;
+		editDlg.scope.data.pdfSinglePage = _oLesson.pdfSinglePage ? true : false;
+		editDlg.scope.data.showAdditional = {};
+		editDlg.scope.data.moduleHelp = moduleHelp;
+		_oLesson.esttime = parseInt(_oLesson.esttime);
+		editDlg.scope.data.lessonData = _oLesson;
+		editDlg.scope.options = {grade: _getDropDown(_dropdownOptions.grades), 
+								 subject: _getDropDown(_dropdownOptions.subjects),
+								 image: _dropdownOptions.icons,
+								 learningMode: learningMode};
+		editDlg.scope.data.grade = {id: _oLesson.grade, name: _oLesson.grade};
+		editDlg.scope.data.subject = {id: _oLesson.subject, name: _oLesson.subject};						 
+		editDlg.scope.data.image = _oLesson.image.indexOf('img:') == 0 ? {id: 'Custom', name: 'Custom'} : {id: _oLesson.image}; 
+		editDlg.scope.data.imageUrl = _oLesson.image.indexOf('img:') == 0 ? _oLesson.image.slice(4, _oLesson.image.length+1) : '';
+		editDlg.scope.data.imageConvert = nl.url.lessonIconUrl(_oLesson.image);
+		editDlg.scope.data.lessonState = lessonStates[_oLesson.state];
+		editDlg.scope.data.learningType = _oLesson.selfLearningMode || _oLesson.selfLearningMode == true ? learningMode[1] : learningMode[0]; 
 	}
-	
+
 	function _getDropDown(arrayElem) {
-		var arrayDict = [];		
+		var arrayDict = [];
 		for(var i=0; i<arrayElem.length; i++) {
-			arrayDict.push({id: arrayElem[i], name: arrayElem[i]});
+			if(arrayElem[i].indexOf(".") > 0) {
+				var getGroup = arrayElem[i].split(".");
+				arrayDict.push({id: arrayElem[i], name: getGroup[1], group: getGroup[0]});
+				continue;
+			}
+			arrayDict.push({id: arrayElem[i], name: arrayElem[i], group: ""});				
 		}
 		return arrayDict;
-	};
+	}
 }]; 
 
 module_init();
