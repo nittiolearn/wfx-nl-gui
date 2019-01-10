73d01eed62f43ba733baa0e63f267487ec95dd58
naveen <naveen@nittio.com>
2019-01-10 15:21:42 +0530

    Error corrections of issue #922

--- www/_code/nittiolesson/page_voice_dlg.js
+++ www/_code/nittiolesson/page_voice_dlg.js
@@ -20,64 +20,64 @@ function(nl, nlDlg, nlResourceAddModifySrv, nlTreeSelect, nlServerApi) {
 	var _oPage = null;
 	var _languageArrayParams = []
 	var _amazonPollyVoicesInfo = [];
-	var _amazonPollyVoices = [{id:'cmn-CN_Zhiyu', name:'Zhiyu', group:'Chinese, Mandarin', subgroup: 'Female'},
-		{id:'da-DK_Naja', name:'Naja', group:'Danish', subgroup: 'Female'},
-		{id:'da-DK_Mads', name:'Mads', group:'Danish', subgroup: 'Male'},
-		{id:'nl-NL_Lotte', name:'Lotte', group:'Dutch', subgroup: 'Female'},
-		{id:'nl-NL_Ruben', name:'Ruben', group:'Dutch', subgroup: 'Male'},
-		{id:'en-AU_Nicole', name:'Nicole', group:'English, Australian', subgroup: 'Female'},
-		{id:'en-AU_Russell', name:'Russell', group:'English, Australian', subgroup: 'Male'},
-		{id:'en-GB_Amy', name:'Amy', group:'English, British', subgroup: 'Female'},
-		{id:'en-GB_Emma', name:'Emma', group:'English, British', subgroup: 'Female'},
-		{id:'en-GB_Brian', name:'Brian', group:'English, British', subgroup: 'Male'},
-		{id:'en-IN_Aditi', name:'Aditi (bilingual with Hindi)', group:'English, Indian', subgroup: 'Female'},
-		{id:'en-IN_Raveena', name:'Raveena', group:'English, Indian', subgroup: 'Female'},
-		{id:'en-US_Ivy', name:'Ivy', group:'English, US', subgroup: 'Female'},
-		{id:'en-US_Joanna', name:'Joanna', group:'English, US', subgroup: 'Female'},
-		{id:'en-US_Kendra', name:'Kendra', group:'English, US', subgroup: 'Female'},
-		{id:'en-US_Kimberly', name:'Kimberly', group:'English, US', subgroup: 'Female'},
-		{id:'en-US_Salli', name:'salli', group:'English, US', subgroup: 'Female'},
-		{id:'en-US_Joey', name:'Joey', group:'English, US', subgroup: 'Male'},
-		{id:'en-US_Justin', name:'Justin', group:'English, US', subgroup: 'Male'},
-		{id:'en-US_Matthew', name:'Matthew', group:'English, US', subgroup: 'Male'},
-		{id:'en-GB-WLS_Geraint', name:'Geraint', group:'English, Welsh', subgroup: 'Male'},
-		{id:'fr-FR_Céline', name:'Céline', group:'French', subgroup: 'Female'},
-		{id:'fr-FR_Léa', name:'Léa', group:'French', subgroup: 'Female'},
-		{id:'fr-FR_Mathieu', name:'Mathieu', group:'French', subgroup: 'Male'},
-		{id:'fr-CA_Chantal', name:'Chantal', group:'French, Canadian', subgroup: 'Female'},
-		{id:'de-DE_Marlene', name:'Marlene', group:'German', subgroup: 'Female'},
-		{id:'de-DE_Vicki', name:'Vicki', group:'German', subgroup: 'Female'},
-		{id:'de-DE_Hans', name:'Hans', group:'German', subgroup: 'Male'},
-		{id:'hi-IN_Aditi', name:'Aditi (bilingual with Indian English)', group:'Hindi', subgroup: 'Female'},
-		{id:'is-IS_Dóra', name:'Dóra/Dora', group:'Icelandic', subgroup: 'Female'},
-		{id:'is-IS_Karl', name:'Karl', group:'Icelandic', subgroup: 'Male'},
-		{id:'it-IT_Carla', name:'Carla', group:'Italian', subgroup: 'Female'},
-		{id:'it-IT_Bianca', name:'Bianca', group:'Italian', subgroup: 'Female'},
-		{id:'it-IT_Giorgio', name:'Giorgio', group:'Italian', subgroup: 'Male'},
-		{id:'ja-JP_Mizuki', name:'Mizuki', group:'Japanese', subgroup: 'Female'},
-		{id:'ja-JP_Takumi', name:'Takumi', group:'Japanese', subgroup: 'Male'},
-		{id:'ko-KR_Seoyeon', name:'Seoyeon', group:'Korean', subgroup: 'Female'},
-		{id:'nb-NO_Liv', name:'Liv', group:'Norwegian', subgroup: 'Female'},
-		{id:'pl-PL_Ewa', name:'Ewa', group:'Polish', subgroup: 'Female'},
-		{id:'pl-PL_Maja', name:'Maja', group:'Polish', subgroup: 'Female'},
-		{id:'pl-PL_Jacek', name:'Jacek', group:'Polish', subgroup: 'Male'},
-		{id:'pl-PL_Jan', name:'Jan', group:'Polish', subgroup: 'Male'},
-		{id:'pt-BR_Vitória', name:'Vitória/Vitoria', group:'Portuguese, Brazilian', subgroup: 'Female'},
-		{id:'pt-BR_Ricardo', name:'Ricardo', group:'Portuguese, Brazilian', subgroup: 'Male'},
-		{id:'pt-PT_Inês', name:'Inês/Ines', group:'Portuguese, European', subgroup: 'Female'},
-		{id:'pt-PT_Cristiano', name:'Cristiano', group:'Portuguese, European', subgroup: 'Male'},
-		{id:'ro-RO_Carmen', name:'Inês/Ines', group:'Romanian', subgroup: 'Female'},
-		{id:'ru-RU_Tatyana', name:'Tatyana', group:'Russian', subgroup: 'Female'},
-		{id:'ru-RU_Maxim', name:'Maxim', group:'Russian', subgroup: 'Male'},
-		{id:'es-ES_Conchita', name:'Conchita', group:'Spanish, European', subgroup: 'Female'},
-		{id:'es-ES_Lucia', name:'Lucia', group:'Spanish, European', subgroup: 'Female'},
-		{id:'es-ES_Enrique', name:'Enrique', group:'Spanish, European', subgroup: 'Male'},
-		{id:'es-MX_Mia', name:'Mia', group:'Spanish, Mexican', subgroup: 'Female'},
-		{id:'es-US_Penélope', name:'Penélope/Penelope', group:'Spanish, US', subgroup: 'Female'},
-		{id:'es-US_Miguel', name:'Miguel', group:'Spanish, US', subgroup: 'Male'},
-		{id:'sv-SE_Astrid', name:'Astrid', group:'Swedish', subgroup: 'Female'},
-		{id:'tr-TR_Filiz', name:'Filiz', group:'Turkish', subgroup: 'Female'},
-		{id:'cy-GB_Gwyneth', name:'Gwyneth', group:'Welsh', subgroup: 'Female'}
+	var _amazonPollyVoices = [{id:'cmn-CN_Zhiyu', name:'Female: Zhiyu', group:'Chinese, Mandarin'},
+		{id:'da-DK_Naja', name:'Female: Naja', group:'Danish'},
+		{id:'da-DK_Mads', name:'Male: Mads', group:'Danish'},
+		{id:'nl-NL_Lotte', name:'Female: Lotte', group:'Dutch'},
+		{id:'nl-NL_Ruben', name:'Male: Ruben', group:'Dutch'},
+		{id:'en-AU_Nicole', name:'Female: Nicole', group:'English, Australian'},
+		{id:'en-AU_Russell', name:'Male: Russell', group:'English, Australian'},
+		{id:'en-GB_Amy', name:'Female: Amy', group:'English, British'},
+		{id:'en-GB_Emma', name:'Female: Emma', group:'English, British'},
+		{id:'en-GB_Brian', name:'Male: Brian', group:'English, British'},
+		{id:'en-IN_Aditi', name:'Female: Aditi (bilingual with Hindi)', group:'English, Indian'},
+		{id:'en-IN_Raveena', name:'Female: Raveena', group:'English, Indian'},
+		{id:'en-US_Ivy', name:'Female: Ivy', group:'English, US'},
+		{id:'en-US_Joanna', name:'Female: Joanna', group:'English, US'},
+		{id:'en-US_Kendra', name:'Female: Kendra', group:'English, US'},
+		{id:'en-US_Kimberly', name:'Female: Kimberly', group:'English, US'},
+		{id:'en-US_Salli', name:'Female: salli', group:'English, US'},
+		{id:'en-US_Joey', name:'Male: Joey', group:'English, US'},
+		{id:'en-US_Justin', name:'Male: Justin', group:'English, US'},
+		{id:'en-US_Matthew', name:'Male: Matthew', group:'English, US'},
+		{id:'en-GB-WLS_Geraint', name:'Male: Geraint', group:'English, Welsh'},
+		{id:'fr-FR_Céline', name:'Female: Céline', group:'French'},
+		{id:'fr-FR_Léa', name:'Female: Léa', group:'French'},
+		{id:'fr-FR_Mathieu', name:'Male: Mathieu', group:'French'},
+		{id:'fr-CA_Chantal', name:'Female: Chantal', group:'French, Canadian'},
+		{id:'de-DE_Marlene', name:'Female: Marlene', group:'German'},
+		{id:'de-DE_Vicki', name:'Female: Vicki', group:'German'},
+		{id:'de-DE_Hans', name:'Male: Hans', group:'German'},
+		{id:'hi-IN_Aditi', name:'Female: Aditi (bilingual with Indian English)', group:'Hindi'},
+		{id:'is-IS_Dóra', name:'Female: Dóra/Dora', group:'Icelandic'},
+		{id:'is-IS_Karl', name:'Male: Karl', group:'Icelandic'},
+		{id:'it-IT_Carla', name:'Female: Carla', group:'Italian'},
+		{id:'it-IT_Bianca', name:'Female: Bianca', group:'Italian'},
+		{id:'it-IT_Giorgio', name:'Male: Giorgio', group:'Italian'},
+		{id:'ja-JP_Mizuki', name:'Female: Mizuki', group:'Japanese'},
+		{id:'ja-JP_Takumi', name:'Male: Takumi', group:'Japanese'},
+		{id:'ko-KR_Seoyeon', name:'Female: Seoyeon', group:'Korean'},
+		{id:'nb-NO_Liv', name:'Female: Liv', group:'Norwegian'},
+		{id:'pl-PL_Ewa', name:'Female: Ewa', group:'Polish'},
+		{id:'pl-PL_Maja', name:'Female: Maja', group:'Polish'},
+		{id:'pl-PL_Jacek', name:'Male: Jacek', group:'Polish'},
+		{id:'pl-PL_Jan', name:'Male: Jan', group:'Polish'},
+		{id:'pt-BR_Vitória', name:'Female: Vitória/Vitoria', group:'Portuguese, Brazilian'},
+		{id:'pt-BR_Ricardo', name:'Male: Ricardo', group:'Portuguese, Brazilian'},
+		{id:'pt-PT_Inês', name:'Female: Inês/Ines', group:'Portuguese, European'},
+		{id:'pt-PT_Cristiano', name:'Male: Cristiano', group:'Portuguese, European'},
+		{id:'ro-RO_Carmen', name:'Female: Inês/Ines', group:'Romanian'},
+		{id:'ru-RU_Tatyana', name:'Female: Tatyana', group:'Russian'},
+		{id:'ru-RU_Maxim', name:'Male: Maxim', group:'Russian'},
+		{id:'es-ES_Conchita', name:'Female: Conchita', group:'Spanish, European'},
+		{id:'es-ES_Lucia', name:'Female: Lucia', group:'Spanish, European'},
+		{id:'es-ES_Enrique', name:'Male: Enrique', group:'Spanish, European'},
+		{id:'es-MX_Mia', name:'Female: Mia', group:'Spanish, Mexican'},
+		{id:'es-US_Penélope', name:'Female: Penélope/Penelope', group:'Spanish, US'},
+		{id:'es-US_Miguel', name:'Male: Miguel', group:'Spanish, US'},
+		{id:'sv-SE_Astrid', name:'Female: Astrid', group:'Swedish'},
+		{id:'tr-TR_Filiz', name:'Female: Filiz', group:'Turkish'},
+		{id:'cy-GB_Gwyneth', name:'Female: Gwyneth', group:'Welsh'}
 	]
 
 	this.init = function(oLesson) {
@@ -114,11 +114,11 @@ function(nl, nlDlg, nlResourceAddModifySrv, nlTreeSelect, nlServerApi) {
 			nlTreeSelect.updateSelectionTree(_amazonPollyVoicesInfo, {}, 0);
 			_amazonPollyVoicesInfo.treeIsShown = false;
 			_amazonPollyVoicesInfo.multiSelect = false;
-			dlgScope.data.autoVoicePolly = [{voiceLanguageInfo: _amazonPollyVoicesInfo, type: {id: 'autovoice'}, text: '', lang: '', voice: '', delay: 0, mp3: '', options: [{id: 'audio', name:'Audio url'}, {id: 'autovoice', name: 'Auto voice'}]}]
+			dlgScope.data.autoVoicePolly = [{voiceLanguageInfo: _amazonPollyVoicesInfo, type: {id: 'autovoice'}, text: '', lang: '', voice: '', delay: 0, mp3: '', options: [{id: 'audio', name:'Audio url'}, {id: 'autovoice', name: 'Automatic voice generation'}]}]
 		}
 		if(_oLesson.autoVoiceProvider && (_oLesson.autoVoiceProvider == 'polly')) {
 			dlgScope.autoVoiceProvider = true;
-			dlgScope.options['pageAudioType'] = [{id: 'audio', name:'Audio url'}, {id: 'autovoice', name: 'Auto voice'}];
+			dlgScope.options['pageAudioType'] = [{id: 'audio', name:'Audio url'}, {id: 'autovoice', name: 'Automatic voice generation'}];
 			dlgScope.data.pageAudioType = dlgScope.options.pageAudioType[1];
 		} else {
 			dlgScope.data.audioUrl = oPage.audioUrl ? oPage.audioUrl : '';
@@ -134,7 +134,7 @@ function(nl, nlDlg, nlResourceAddModifySrv, nlTreeSelect, nlServerApi) {
 
 		dlgScope.data.addPollyVoiceField = function() {
 			if(dlgScope.data.autoVoicePolly.length == 0) {
-				dlgScope.data.autoVoicePolly.push({voiceLanguageInfo: angular.copy(_amazonPollyVoicesInfo), type:{id: 'autovoice'}, text: '', lang: '', voice: '', delay: 0, mp3: '', options: [{id: 'audio', name:'Audio url'}, {id: 'autovoice', name: 'Auto voice'}]});
+				dlgScope.data.autoVoicePolly.push({voiceLanguageInfo: angular.copy(_amazonPollyVoicesInfo), type:{id: 'autovoice'}, text: '', lang: '', voice: '', delay: 0, mp3: '', options: [{id: 'audio', name:'Audio url'}, {id: 'autovoice', name: 'Automatic voice generation'}]});
 				return;
 			}
 			var lastSelected = dlgScope.data.autoVoicePolly[dlgScope.data.autoVoicePolly.length - 1];
@@ -147,7 +147,7 @@ function(nl, nlDlg, nlResourceAddModifySrv, nlTreeSelect, nlServerApi) {
 
 			if(lastSelected.type.id == "voice" && !lastSelected.text) 
 				return nlDlg.popupAlert({title: 'Error message', template: 'Please add the text for autovoice'});
-			dlgScope.data.autoVoicePolly.push({voiceLanguageInfo: angular.copy(_amazonPollyVoicesInfo), type:{id: 'autovoice'}, text: '', lang: '', voice: '', delay: 0, mp3: '', options:[{id: 'audio', name:'Audio url'}, {id: 'autovoice', name: 'Auto voice'}]});
+			dlgScope.data.autoVoicePolly.push({voiceLanguageInfo: angular.copy(lastSelected.voiceLanguageInfo), type:{id: 'autovoice'}, text: '', lang: '', voice: '', delay: 0, mp3: '', options:[{id: 'audio', name:'Audio url'}, {id: 'autovoice', name: 'Automatic voice generation'}]});
 		};
 
 		dlgScope.selectAudioUrl = function(index) {
@@ -155,10 +155,14 @@ function(nl, nlDlg, nlResourceAddModifySrv, nlTreeSelect, nlServerApi) {
 			if(selectedItem.type.id == 'autovoice') return;
 			nlResourceAddModifySrv.insertOrUpdateResource(parentScope, 
 				_restypes, 'audio:' + dlgScope.data.audioUrl, false, _resourceDict, false, _lessonId).then(function(result) {
-					if(result && result.url) {
-						dlgScope.data.audioUrl = result.url;
-						selectedItem['type'] = {id: 'audio'};
-						selectedItem['mp3'] = result.url;
+				if(result && result.url) {
+					dlgScope.data.audioUrl = result.url;
+					selectedItem['type'] = {id: 'audio'};
+					selectedItem['mp3'] = result.url;
+				} else {
+					if(!selectedItem['mp3']) {
+						selectedItem['type'] = {id: 'autovoice'};
+					}
 				}
 			});
 		}
@@ -176,6 +180,7 @@ function(nl, nlDlg, nlResourceAddModifySrv, nlTreeSelect, nlServerApi) {
 					oPage.audioUrl = dlgScope.data.audioUrl;
 					oPage.autoVoice = dlgScope.data.autoVoice;
 					resolve(oPage);
+					nlDlg.closeAll();
 				}
 			}
 		};
@@ -211,19 +216,15 @@ function(nl, nlDlg, nlResourceAddModifySrv, nlTreeSelect, nlServerApi) {
 			var fragment = _oPage.autoVoicePolly[i];
 			var arrayParams = angular.copy(_languageArrayParams);
 			var _amazonPollyVoicesInfo = {data: arrayParams[0] || []};
+			var selectedIds = {};
 			if(fragment.type == 'autovoice') {
 				var selectedId = fragment.lang+'_'+fragment.voice ;
-				var selectedIds = _getLeafNodeIds(selectedId);
-				nlTreeSelect.updateSelectionTree(_amazonPollyVoicesInfo, selectedIds, 0);
-				_amazonPollyVoicesInfo.treeIsShown = false;
-				_amazonPollyVoicesInfo.multiSelect = false;
-				dlgScope.data.autoVoicePolly.push({voiceLanguageInfo: _amazonPollyVoicesInfo, delay: fragment.delay, type: {id: 'autovoice'}, text: fragment.text, lang: fragment.lang, voice: fragment.voice, mp3: fragment.mp3, options: [{id: 'audio', name:'Audio url'}, {id: 'autovoice', name: 'Auto voice'}]});
-			} else {
-				nlTreeSelect.updateSelectionTree(_amazonPollyVoicesInfo, {}, 0);
-				_amazonPollyVoicesInfo.treeIsShown = false;
-				_amazonPollyVoicesInfo.multiSelect = false;
-				dlgScope.data.autoVoicePolly.push({voiceLanguageInfo: _amazonPollyVoicesInfo, delay: fragment.delay, type: {id: 'audio'}, text: fragment.text, lang: fragment.lang || '', voice: fragment.voice || '', mp3: fragment.mp3, options: [{id: 'audio', name:'Audio url'}, {id: 'autovoice', name: 'Auto voice'}]});
+				selectedIds = _getLeafNodeIds(selectedId);
 			}
+			nlTreeSelect.updateSelectionTree(_amazonPollyVoicesInfo, selectedIds, 0);
+			_amazonPollyVoicesInfo.treeIsShown = false;
+			_amazonPollyVoicesInfo.multiSelect = false;
+			dlgScope.data.autoVoicePolly.push({voiceLanguageInfo: _amazonPollyVoicesInfo, delay: fragment.delay || 0, type: {id: fragment.type}, text: fragment.text || '', lang: fragment.lang || '', voice: fragment.voice || '', mp3: fragment.mp3, options: [{id: 'audio', name:'Audio url'}, {id: 'autovoice', name: 'Automatic voice generation'}]});
 		}
 	}
 
@@ -262,10 +263,10 @@ function(nl, nlDlg, nlResourceAddModifySrv, nlTreeSelect, nlServerApi) {
 			}
 			var selectedLang = null;
 			if(selectedVoice) {
-				selectedVoice = selectedVoice.split('_');
-				selectedLang = selectedVoice[0].split('.');
+				selectedVoice = selectedVoice.split('.');
+				selectedLang = selectedVoice[1].split('_');
 			}
-			newAutovoiceArray.push({type: fragment.type.id, text: fragment.text || '', lang: selectedLang ? selectedLang[2] : '', voice: selectedVoice ? selectedVoice[1] : '', mp3:fragment.mp3 || '', delay: fragment.delay || 0});
+			newAutovoiceArray.push({type: fragment.type.id, text: fragment.text || '', lang: selectedLang ? selectedLang[0] : '', voice: selectedLang ? selectedLang[1] : '', mp3:fragment.mp3 || '', delay: fragment.delay || 0});
 		}
 		return true;
 	}
@@ -283,16 +284,11 @@ function(nl, nlDlg, nlResourceAddModifySrv, nlTreeSelect, nlServerApi) {
     }
 
     function _getIconNodeWithParents(itemObj, treeArray, insertedKeys) {
-		var subgroupid = itemObj.group+'.'+itemObj.subgroup;
         if (itemObj.group && !insertedKeys[itemObj.group]) {
         	insertedKeys[itemObj.group] = true;
         	treeArray.push({id: itemObj.group, name: itemObj.group, isOpen: true});
 		}
-		if (itemObj.subgroup && !insertedKeys[subgroupid]) {
-        	insertedKeys[subgroupid] = true;
-        	treeArray.push({id: subgroupid, name: itemObj.subgroup, isOpen: true});
-		}
-		var itemId = (itemObj.group && itemObj.subgroup) ? subgroupid + '.' + itemObj.id : itemObj.id;
+		var itemId = itemObj.group ? itemObj.group + '.' + itemObj.id : itemObj.id;
 		itemIdDict[itemId] = true;
         if (insertedKeys[itemId]) return;
     	insertedKeys[itemId] = true;        
@@ -301,8 +297,11 @@ function(nl, nlDlg, nlResourceAddModifySrv, nlTreeSelect, nlServerApi) {
 
     function _getHelp() {
         return {
-			pageAudioType: {name: nl.t('Play audio from'), help: nl.t('Please select the mode of audio to be added to this page.')},
-            audioUrl: {name: nl.t('Page Audio URL'), help: nl.t('Provide background audio to play when the page is displayed')},
+			pageAudioType: {name: nl.t('Play audio from'), help: nl.t('Please select the mode of audio to be added to this page.'), isShown: false},
+			languageInfo: {name: nl.t('Select the language'), help: nl.t('Please select the mode of audio to be added to this page.'), isShown: false},
+			audioUrl: {name: nl.t('Page Audio URL'), help: nl.t('Provide background audio to play when the page is displayed')},
+			voicetext: {name: nl.t('Autovoice text'), help: nl.t('Provide the autovoice to play for the page.'), isShown: false},
+			delay: {name: nl.t('Voice delay(seconds)'), help: nl.t('Delay before the voice is played.'), isShown: false},
             autoVoice: {name: nl.t('Page Voice Script'), 
             	help: nl.t('<p>Provide text that should be played as audio when the page is displayed.</p>'
                 + '<p>Keep each line short. Use ",", "." and new line to control the pauses.</p>'
