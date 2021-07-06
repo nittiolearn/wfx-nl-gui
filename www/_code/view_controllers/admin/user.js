5899a8c8c013fdb83ff052110ff78b2a28d6231a
Aazar <44za12@gmail.com>
2021-07-07 03:56:40 +0530

    #2165 first commit

--- www/_code/view_controllers/admin/user.js
+++ www/_code/view_controllers/admin/user.js
@@ -109,6 +109,8 @@ nlAdminUserExport, nlAdminUserImport, nlTreeSelect, nlOuUserSelect, nlServerApi)
             advancedProp(card);
         } else if (linkid === 'adminuser_unarchive') {
             _unarchive();
+        } else if (linkid === 'adminuser_status') {
+            _status();
         }
     };
 
@@ -181,6 +183,8 @@ nlAdminUserExport, nlAdminUserImport, nlTreeSelect, nlOuUserSelect, nlServerApi)
             card.children.push({title: nl.t('Unarchive'), internalUrl: 'adminuser_unarchive',
             children: [], link: [], style: 'nl-create-card'});
         }
+        card.children.push({title: nl.t('User status'), internalUrl: 'adminuser_status',
+            children: [], link: [], style: 'nl-create-card'});
 		return ret;
 	}
 
@@ -353,6 +357,30 @@ nlAdminUserExport, nlAdminUserImport, nlTreeSelect, nlOuUserSelect, nlServerApi)
             nlDlg.closeAll();
             nl.window.location.reload();
         });
+    }
+
+    function _status() {
+        var dlg = nlDlg.create($scope);
+        dlg.setCssClass('nl-height-max nl-width-max');
+        dlg.scope.error = {};
+        dlg.scope.dlgTitle = nl.t('Users Status');
+        var data = {total: {active: 0, inactive: 0}}
+        for (var uid in _groupInfo.users){
+            var userObj = nlGroupInfo.getUserObj(uid,_grpid)
+            var userTypeStr = userObj.getUtStr()
+            if (!data[userTypeStr]){
+                data[userTypeStr] = {active: userObj.state ? 1 : 0, inactive: userObj.state ? 0 : 1};
+                userObj.state ? data.total.active++ : data.total.inactive++;
+            } else {
+                userObj.state ? data[userTypeStr].active++ : data[userTypeStr].inactive++;
+                userObj.state ? data.total.active++ : data.total.inactive++;
+            }
+        }
+        dlg.scope.usersStatus = data;
+        var cancelButton = {
+            text : nl.t('Close')
+        };
+        dlg.show('view_controllers/admin/user_status_dlg.html', [], cancelButton, false);
     }
 
     function _createOrModify(card) {
